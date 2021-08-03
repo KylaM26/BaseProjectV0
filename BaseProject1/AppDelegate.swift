7f0d7933726c068b3a6bd9caa53db8d3892eae82
Kyla <TheRealKylaMae@gmail.com>
2021-08-02 20:41:50 -0500

    Initial commit

---
+++ BaseProject1/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  BaseProject1
+//
+//  Created by Kyla Wilson on 8/2/21.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ BaseProject1/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  BaseProject1
+//
+//  Created by Kyla Wilson on 8/2/21.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+
+    }
+
+    override var representedObject: Any? {
+        didSet {
+
+        }
+    }
+}
+
